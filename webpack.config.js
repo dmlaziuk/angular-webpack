aca7984c3f7a8c426c1d397c51d97dcec1593182
dmlaziuk <dm.laziuk@gmail.com>
2018-08-22 00:28:59 +0300

    Initial commit
    - created boilerplate for babel + webpack + react
    - configured eslint

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['env'] }
+      }
+    ]
+  }
+}
